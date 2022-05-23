ff85f872264fef8a31029efc9197dbb7a3136a25
danilolopesabreu <danilo.bcc@gmail.com>
2022-05-23 11:26:10 -0300

    add mouse move

---
+++ mouse_move/mouse.py
@@ -0,0 +1,23 @@
+"""
+!pip install pyautogui
+"""
+
+import pyautogui
+print(pyautogui.size())
+
+pyautogui.moveTo(100, 100, duration = 1)
+
+import numpy as np
+w = np.random.randint(0, 1919)
+h = np.random.randint(0, 1079)
+
+pyautogui.moveTo(w, h, duration = 1)
+
+import time
+
+while True:
+    w = np.random.randint(0, 1919)
+    h = np.random.randint(0, 1079)
+    pyautogui.moveTo(w, h, duration = 1)
+    time.sleep(5)  #time in seconds
+    
